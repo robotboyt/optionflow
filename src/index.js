90cc868df3ac38c4c72825891654b2e10f6c6bbc
robotboyt <bokachodantes@gmail.com>
2024-01-10 16:54:54 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
